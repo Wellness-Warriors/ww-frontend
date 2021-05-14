b0cb0bc388045f09e7055b68bd1b52a0848ef49e
antoni <lorenzo.antonio.090@gmail.com>
2021-05-13 21:43:22 -0700

    craete home component and saved entries component

---
+++ src/SavedEntries.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+class SavedEntries extends React.Component{
+  render(){
+    return(
+
+      <Card>
+        <h1>Saved Entries Component</h1>
+      </Card>
+
+    );
+  }
+}
+
+export default SavedEntries;
