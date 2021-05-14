b7ee534fab8293db16f3cbc4d61989f698774349
antoni <lorenzo.antonio.090@gmail.com>
2021-05-14 11:01:55 -0700

    craete zen req

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import axios from 'axios';
 import NavBar from './NavBar';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 
 class Home extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      quote: '',
+      author: '',
+    };
+  }
+  componentDidMount(){
+    this.getZen();
+  }
+
+  getZen = async() =>{
+    try{
+
+      let zenData = await axios.get(`{process.env.REACT_APP_BACKEND_URL}/zen`);
+      console.log('response:',zenData);
+      console.log('response:',zenData.quote,zenData.author);
+
+    }catch(error){
+      console.log(error);
+    }
+  }
 
   onChangeHandler = (e) => {
     e.preventDefault();
